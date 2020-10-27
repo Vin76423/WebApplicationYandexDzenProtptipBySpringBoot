485f3aaf38cbfd26176f40d2bfd96bd0f92048ee
OlegVilchynsky <vin76423@gmail.com>
2020-10-27 18:43:44 +0300

    Add interceptor configuration update InterceptorConfiguration.java

--- src/main/java/com/example/webapplicationboot/configuration/InterceptorConfiguration.java
+++ src/main/java/com/example/webapplicationboot/configuration/InterceptorConfiguration.java
@@ -1,5 +1,8 @@
 package com.example.webapplicationboot.configuration;
 
+import com.example.webapplicationboot.interceptor.HomeInterceptor;
+import com.example.webapplicationboot.interceptor.PostInterceptor;
+import com.example.webapplicationboot.interceptor.PostLikeInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,13 +14,13 @@ public class InterceptorConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new PostInterceptor())
-//                .addPathPatterns("/post/create");
-//        registry.addInterceptor(new HomeInterсeptor())
-//                .addPathPatterns("/home/reg")
-//                .addPathPatterns("/home/auth");
-//        registry.addInterceptor(new PostLikeInterceptor())
-//                .addPathPatterns("/post/set-like")
-//                .addPathPatterns("/post/like");
+        registry.addInterceptor(new PostInterceptor())
+                .addPathPatterns("/post/create");
+        registry.addInterceptor(new HomeInterceptor())
+                .addPathPatterns("/home/reg")
+                .addPathPatterns("/home/auth");
+        registry.addInterceptor(new PostLikeInterceptor())
+                .addPathPatterns("/post/set-like")
+                .addPathPatterns("/post/like");
     }
 }
